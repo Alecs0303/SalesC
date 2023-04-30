cd67bef404e8f44f5e78eadf79d0ffcbae53be41
Alecs0303 <alecsa2009@hotmail.com>
2023-04-30 09:44:00 -0500

    WEB and API were linked

---
+++ Sales.WEB/Repostories/IRepository.cs
@@ -0,0 +1,12 @@
+﻿namespace Sales.WEB.Repostories
+{
+    public interface IRepository
+    {
+        Task<HttpResponseWrapper<T>> Get<T>(string url);
+
+        Task<HttpResponseWrapper<object>> Post<T>(string url, T model);
+
+        Task<HttpResponseWrapper<TResponse>> Post<T, TResponse>(string url, T model);
+    }
+
+}
